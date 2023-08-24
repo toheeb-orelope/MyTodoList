6093cbfb85d55072a2417aa6ca47d4242abed5a1
Toheeb <adeptadept@icloud.com>
2023-08-24 09:49:00 +0100

    Initial Commit

---
+++ MyTodoList/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by Adept Adept on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
